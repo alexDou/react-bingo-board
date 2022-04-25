d7de8846090e916feff1532f6e4df07800f38681
alex doo <aldou@aldoo.local>
2022-04-25 18:33:30 +0700

    random range

--- src/api/api.config.ts
+++ src/api/api.config.ts
@@ -2,7 +2,7 @@ import { APIConfig } from "./types";
 
 const apiConfig: APIConfig = {
     endpoint: 'https://www.randomnumberapi.com/api/v1.0/random',
-    payload: { min:10, max: 24, count: 1 }
+    payload: { min:1, max: 24, count: 1 }
 } as const;
 
 export default apiConfig;
