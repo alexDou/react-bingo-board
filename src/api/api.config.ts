86e3a972a5c86e129ec9a9cc0904039a82cdfcfc
alex doo <aldou@aldoo.local>
2022-04-25 18:16:24 +0700

    https

--- src/api/api.config.ts
+++ src/api/api.config.ts
@@ -1,7 +1,7 @@
 import { APIConfig } from "./types";
 
 const apiConfig: APIConfig = {
-    endpoint: 'http://www.randomnumberapi.com/api/v1.0/random',
+    endpoint: 'https://www.randomnumberapi.com/api/v1.0/random',
     payload: { min:10, max: 24, count: 1 }
 } as const;
 
